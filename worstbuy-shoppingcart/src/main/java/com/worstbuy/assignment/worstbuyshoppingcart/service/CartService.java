49cf1e9d54b1baf7e7527be5312ac26e91eb3b5d
Sumukh Deshpande <sumukhmailings@gmail.com>
2018-05-22 13:26:16 +0530

    Removed unwanted files.

--- worstbuy-shoppingcart/src/main/java/com/worstbuy/assignment/worstbuyshoppingcart/service/CartService.java
+++ worstbuy-shoppingcart/src/main/java/com/worstbuy/assignment/worstbuyshoppingcart/service/CartService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.worstbuy.assignment.worstbuyshoppingcart.dao.ProductDao;
 import com.worstbuy.assignment.worstbuyshoppingcart.exception.ValidationException;
 import com.worstbuy.assignment.worstbuyshoppingcart.model.Cart;
 import com.worstbuy.assignment.worstbuyshoppingcart.model.Product;
